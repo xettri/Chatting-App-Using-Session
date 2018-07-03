20d21c03e39ce271681501888eefb0979e45188f
Ccrazydreamer <bcrazydreamer@gmail.com>
2018-07-03 07:34:24 +0530

    AI Chat added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser                              = require('body-parser');
 var session                                 = require("express-session");
 var SessionStore                            = new session.MemoryStore();
 var helper                                  = require('./helper');
+var AIChat                                  = require('./AIChat');
 var schedule                                = require('node-schedule');
 
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname,"public")));
 
 //-----------------
 dmsg={};
+pmsg={};
 //-----------------
 
 app.use(function(req, res, next) {
@@ -47,7 +49,18 @@ app.get('/', function(req, res)
   res.render('Index');
 });
 
-app.post("/",function(req,res)
+app.get('/globalChat', function(req, res)
+{
+  res.render('ChatRoom/PublicRoom/Index',{title:'DChat-GlobalChat'});
+});
+
+app.get('/privateChat', function(req, res)
+{
+  res.render('ChatRoom/AI/Index',{title:'DChat-PrivateChat'});
+});
+
+//--------------------------------------------------------------------------------------
+app.post("/GlobalChat",function(req,res)
 {
   	var data=req.body;
   	user=data.username;
@@ -55,25 +68,45 @@ app.post("/",function(req,res)
   	req.session.user=key;
   	dmsg[req.session.user]=[helper.AppConstant.colors[Math.floor(Math.random() * 14)]];
     dmsg[req.session.user][1]=user;
-    dmsg[req.session.user][2]='#DCE3DA'; //for ai;
-    res.render('ChatRoom/PublicRoom/chat');
+    console.log(req.session.user);
+    res.render('ChatRoom/PublicRoom/chat',{user:req.session.user});
 });
 
+app.post("/privateChat",function(req,res)
+{
+    var data=req.body;
+    user=data.username;
+    var key =helper.Common.genKey(user);
+    req.session.user=key;
+    pmsg[req.session.user]=[helper.AppConstant.colors[Math.floor(Math.random() * 14)]];
+    pmsg[req.session.user][1]=user;
+    ai={};
+    ai.uname=helper.AppConstant.AiNames[Math.floor(Math.random() * 10)];
+    ai.ucolor='#FFCDD2';
+    ai.count=0;
+    pmsg[req.session.user+'_ai']=ai;
+    console.log(ai);
+    res.render('ChatRoom/AI/chat',{user:req.session.user});
+});
+
+//------------------------------------------------------
+app.get('/private/logout', function(req, res){
+    delete pmsg[req.session.user];
+    delete pmsg[req.session.user+'_ai']
+    res.redirect("/");
+});
 app.get('/logout', function(req, res){
-		dmsg[req.session.user]="";
-		req.session.user==undefined;
-		dmsg[req.session.user][1]="";
-		if(dmsg.message!=null || dmsg.message!=undefined){dmsg.message="";}
+		delete dmsg[req.session.user];
 		res.redirect("/");
 });
-
+//-----------------------------------------------------
 app.get('/about', function(req, res){
   res.render('AppPages/about');
 });
 
 //------------------------------------------------------------------------
 io.on('connection', function(socket){
-  socket.on('dchat msg', function(msg){
+  socket.on('dchat_msg_'+socket.request.session.user, function(msg){
       dmsg.message=msg;
       try
       {
@@ -84,22 +117,33 @@ io.on('connection', function(socket){
       {
         dmsg.uname="Anonymous";
       }
-      io.emit('dchat msg', dmsg);
-      io.emit('dchat msg', dmsg);
+      io.emit('dchat_msg_global', dmsg);
     });
     //-----------------------------------------------------
-    socket.on('dchat_ai_msg', function(msg){
-        dmsg.message=msg;
+    socket.on('dchat_ai_msg_'+socket.request.session.user, function(msg){
+        pmsg.message=msg;
         try
         {
-          dmsg.ucolor=dmsg[socket.request.session.user][2];
-          dmsg.uname=dmsg[socket.request.session.user][1];
+          pmsg.ucolor=pmsg[socket.request.session.user][0];
+          pmsg.uname=pmsg[socket.request.session.user][1];
         }
         catch(err)
         {
           dmsg.uname="Anonymous";
         }
-        io.emit('dchat_ai_msg', dmsg);
+          io.emit('dchat_ai_msg_'+socket.request.session.user, pmsg);
+          AI_Reply = pmsg[socket.request.session.user+'_ai'];
+          pmsg[socket.request.session.user+'_ai'].count++;
+          if(pmsg[socket.request.session.user+'_ai'].count==15)
+          {
+            last=['Ok bey','i have work by','baad me baat karti hu cu','bey bey','tata cu','ok g cu','bey'+pmsg.uname]
+            AI_Reply.message = last[Math.floor(Math.random() * 7)]
+            io.emit('dchat_ai_msg_'+socket.request.session.user, AI_Reply);
+          } else if(pmsg[socket.request.session.user+'_ai'].count<15){
+            AI_Reply.message=AIChat.Common.QuickReply(pmsg.uname,pmsg.message);
+            io.emit('dchat_ai_msg_'+socket.request.session.user, AI_Reply);
+          }
+
       });
 
 });
@@ -107,6 +151,7 @@ io.on('connection', function(socket){
 
 var j = schedule.scheduleJob('0 * * *', function(){
   dmsg={};
+  pmsg={};
 });
 
 app.get('*', function(req, res){
--- helper/Constants/AppConstant.js
+++ helper/Constants/AppConstant.js
@@ -1,8 +1,9 @@
 colors=["#FFCDD2","#F50057","#9C27B0","#E040FB","#651FFF","#3D5AFE","#1E88E5","#00B0FF","#76FF03","#AEEA00","#FFC400","#FF6E40","#B0BEC5","#FFC107"];
+AiColor='#FFCDD2';
 
-
-
+AiNames=['Anamika Singh','Anjali Singh','Jiya Thakur','Aditi Sharma','Binu','Divya','Eva','Gori','Hina','Ishika Khan'];
 
 module.exports = {
   'colors'                  : colors,
+  'AiNames'                 : AiNames,
 }
